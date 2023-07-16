71182f25382dca8649278b2079e6bebfbf3b064f
Africanboy Kiima <fadhilkiima@gmail.com>
2023-07-16 09:52:49 +0100

    Created sqlalchemy db url to connect it to db, also instantiated Base model from which all models will inherit

---
+++ app/database.py
@@ -0,0 +1,17 @@
+#IMPORTING SQLALCHEMY PARTS
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/fastapi'
+
+
+#The engine allows us to connect to our database in postgresql
+engine = create_engine(SQLALCHEMY_DATABASE_URL)#Here sqlalchemy is now connected to our database
+
+#Sessions allow our sqlalchemy to communicate with our database hence its now able to communicate with the database that will ofcoure contain our tables
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind= engine)
+
+#Parent class from which all models will inherit
+Base = declarative_base()
