28865ba123c14a4ba3e6f3deb33fdd60fa94247c
Africanboy Kiima <fadhilkiima@gmail.com>
2023-08-16 15:42:03 +0100

    Just commit for the sake of commiting

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ from sqlalchemy.sql.expression import Null
 
 class Post(Base):#post model extends base
     __tablename__ = "posts"
-    
     id = Column(Integer, primary_key = True,nullable = False)
     title = Column(String, nullable= False)
     content = Column(String, nullable = False)
