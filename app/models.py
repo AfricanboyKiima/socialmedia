88d80d7e3b729a4a17fbd730a42a89f6df424b16
Africanboy Kiima <fadhilkiima@gmail.com>
2023-08-26 20:22:26 +0100

    Removed repeated called Column from import

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column,Integer,String,Column,Boolean
+from sqlalchemy import Column,Integer,String,Boolean
 from sqlalchemy.sql.expression import Null
 
 
@@ -9,4 +9,4 @@ class Post(Base):#post model extends base
     id = Column(Integer, primary_key = True,nullable = False)
     title = Column(String, nullable= False)
     content = Column(String, nullable = False)
-    published = Column(Boolean, default = True)
+    published = Column(Boolean, server_default="TRUE",nullable=False)
