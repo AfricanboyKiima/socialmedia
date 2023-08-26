a59eb03a274d42fecc3dc7dfa48e33bd59844c2a
Africanboy Kiima <fadhilkiima@gmail.com>
2023-08-26 21:32:31 +0100

    Added created_at column to include date and time at which a post is created

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
 from sqlalchemy import Column,Integer,String,Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Post(Base):#post model extends base
     __tablename__ = "posts"
@@ -8,4 +9,4 @@ class Post(Base):#post model extends base
     title = Column(String, nullable= False)
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default="TRUE",nullable=False)
-    created_at = Column()
+    created_at = Column(TIMESTAMP(timezone=True),nullable = False,server_default=text('now()'))
