5cb7c285d798fbf15ccb1d7ca178c6c007f6c1ea
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-04 22:32:03 +0100

    Renamed SessionLocal to SessionFactory to show that it is responsible for creating new sessions

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/fastapi'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)#Here sqlalchemy is now connected to our database
 
 #Sessions allow our sqlalchemy to communicate with our database hence its now able to communicate with the database that will ofcoure contain our tables
-SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind= engine)#Session factory is used to create a session factory that's responsible for creating a function to create new sessions
+SessionFactory = sessionmaker(autocommit = False, autoflush=False, bind= engine)#Session factory is used to create a session factory that's responsible for creating a function to create new sessions
 #A database session is used to interact with the database and perform operations in it
 
 #Parent class from which all models will inherit
@@ -20,7 +20,7 @@ Base = declarative_base()
 
 #creates a database seesion and returns it
 def get_db():
-    db = SessionLocal()#instantiate session objects for each user
+    db = SessionFactory()#instantiate session objects for each user
     try:
         yield db
     finally:
