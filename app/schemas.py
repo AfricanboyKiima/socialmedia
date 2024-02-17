98b5b0550bb66bc7b7559842036efbad1cb8a534
Africanboy Kiima <fadhilkiima@gmail.com>
2024-02-17 09:42:25 +0100

    Added schema that defines the structure of a response to the client

--- app/schemas.py
+++ app/schemas.py
@@ -7,5 +7,16 @@ class PostBase(BaseModel):
     content:str
     published: bool = True
 
+#Defines structure of how data should be sent from the frontend
 class PostCreate(PostBase):
     pass #helps avoid errors in our class 
+
+
+#Defines structure of how data should be sent to the user or how a response should be structured
+class PostResponse(BaseModel):
+    title:str
+    content:str
+    published:bool
+
+    class Config:
+        orm_mode = True
