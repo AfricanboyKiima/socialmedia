f1543189d791805d4f9ed9ac1e446bd041bf4982
Africanboy Kiima <fadhilkiima@gmail.com>
2024-02-17 15:41:15 +0100

    Added orm_mode attribute to config class to tell pydantic model to read the data even when its not a dictionnary

--- app/schemas.py
+++ app/schemas.py
@@ -14,9 +14,10 @@ class PostCreate(PostBase):
 
 #Defines structure of how data should be sent to the user or how a response should be structured
 class PostResponse(BaseModel):
+    id:int
     title:str
     content:str
     published:bool
 
     class Config:
-        orm_mode = True
+        orm_mode = True# pydantic's orm_mode tells the pydantinc model to read the data even if it's not a dict
