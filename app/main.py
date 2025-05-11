a7e46607efa23889ed0ffb3538f23cd8175f1583
Africanboy Kiima <fadhilkiima@gmail.com>
2025-05-11 21:57:24 +0100

    Corrected the status code detail by adding the f

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ def get_users(db:Session = Depends(get_db)):
 def get_user(id:int, db:Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if user is None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail="user with id {id} doesn't exist ")
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail=f"user with id {id} doesn't exist ")
     return user
 
 
