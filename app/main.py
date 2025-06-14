130c6c956114b1fe5b08136eeb09095f28de52b2
Africanboy Kiima <fadhilkiima@gmail.com>
2025-06-14 22:17:34 +0100

    included auth router

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
-from . import database,models
-from app.routers import post,user
-models.Base.metadata.create_all(bind = database.engine)
+
+from app.database import database
+from app.routers import post,user, auth
+
+from app.models import posts as post_model
+from app.models import users as user_model
+
+
+
+database.Base.metadata.create_all(bind = database.engine)
+
+
 
 
 app = FastAPI() 
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 
