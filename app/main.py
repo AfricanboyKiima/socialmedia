ba7a3916396acdd871af174246b552d2dd2e51c6
Africanboy Kiima <fadhilkiima@gmail.com>
2025-06-19 10:44:10 +0100

    Deleted objects in main and implemented them in models init file

--- app/main.py
+++ app/main.py
@@ -3,10 +3,6 @@ from fastapi import FastAPI
 from app.database import database
 from app.routers import post,user, auth
 
-from app.models import posts as post_model
-from app.models import users as user_model
-
-
 
 database.Base.metadata.create_all(bind = database.engine)
 
