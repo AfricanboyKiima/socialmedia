2ee43709b64f631743d0c9ec4336ae7e3017ac20
Africanboy Kiima <fadhilkiima@gmail.com>
2025-07-09 13:37:32 +0100

    Commented code

--- app/utils.py
+++ app/utils.py
@@ -3,11 +3,12 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
 
 
-#hash password
-def password_hasher(password:str):
+
+def password_hasher(password:str): #hash password
     hashed_password = pwd_context.hash(password)
     return hashed_password
 
-def verify(plain_password, hashed_password):
+
+def verify(plain_password, hashed_password): #verify if incoming password matches its hash in the database
     verified_password = pwd_context.verify(plain_password, hashed_password)
     return verified_password
